27a9a9acc9a3787d1819b93663ab9e4596400ff9
Luiso-o <cheportillo@gmail.com>
2024-08-29 19:46:18 +0200

    modificaciones JavaFX

--- src/main/java/com/java/fx/Main.java
+++ src/main/java/com/java/fx/Main.java
@@ -6,24 +6,26 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Main extends Application {
 
+	public static ConfigurableApplicationContext context;
+
 	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
 		launch();
+		SpringApplication.run(Main.class, args);
 	}
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		var context = SpringApplication.run(Main.class);
-		var fxml = new FXMLLoader(getClass().getResource("/Main.fxml"));
-		var scene = new Scene(fxml.load());
+		context = SpringApplication.run(Main.class);
+		FXMLLoader fxml = new FXMLLoader(getClass().getResource("/Main.fxml"));
+		fxml.setControllerFactory(context::getBean);
 
-		String titulo = context.getBean("titulo", String.class);
-
-		stage.setTitle(titulo);
+		Scene scene = new Scene(fxml.load());
+		stage.setTitle("*CRUD - Java FX y Spring Boot ");
 		stage.setScene(scene);
 		stage.show();
 	}
